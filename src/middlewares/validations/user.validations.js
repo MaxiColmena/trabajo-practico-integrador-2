6fd8736ebec8adbd97b7f45f5f0b65e49284cb6c
MaxiColman <colmanmaximo2003@gmail.com>
2025-09-25 00:53:35 -0600

    Se realizaron las validaciones de user

--- src/middlewares/validations/user.validations.js
+++ src/middlewares/validations/user.validations.js
@@ -1,2 +1,49 @@
-import { UserModel } from "../../models/user.model.js";
+import { body } from "express-validator";
 
+export const createUserValidation = [
+  body("useName")
+    .notEmpty()
+    .isString()
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Debe ingresar un nombre de usuario valido"),
+  body("email")
+    .notEmpty()
+    .isString()
+    .isEmail()
+    .withMessage("Debe ingresar un email valido"),
+  body("password")
+    .notEmpty()
+    .isString()
+    .withMessage("Debe ingresar una contraseña valida"),
+  body("role")
+    .notEmpty()
+    .isIn(["user", "admin"])
+    .withMessage("Debe ingresar un rol valido (admin, user)"),
+  body("profile.firstname")
+    .notEmpty()
+    .isString()
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Debe ingresar un nombre valido"),
+  body("profile.lastname")
+    .notEmpty()
+    .isString()
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Debe ingresar un apellido valido"),
+  body("profile.biography")
+    .optional()
+    .isString()
+    .isLength({ max: 500 })
+    .withMessage("Debe ingresar una biografia valida"),
+  body("profile.avatarUrl")
+    .optional()
+    .isString()
+    .isURL()
+    .withMessage("Debe ingresar una URL valida del avatar"),
+  body("profile.birthdate")
+    .optional()
+    .isString()
+    .isDate()
+    .withMessage("Debe ingresar una fecha valida"),
+];
+
+export const updateUserValidation = [];
